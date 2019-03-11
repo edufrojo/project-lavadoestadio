2e600f20e53ac37fb118d225459c3ba7f9ccdd07
edufrojo <edufrojo@gmail.com>
2019-03-11 09:29:44 +0100

    feat(general): crear repositorio

---
+++ app.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// Dependencias
+const http = require('http');
+const express = require('express');
+const logger = require('morgan');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const request = require('request');
+const mail = require('nodemailer').mail;
+
+// Creamos servidor y configuramos el puerto
+const app = express();
+app.set('port', process.env.PORT || 8080);
+
+// Habilitamos log.
+app.use(logger('combined'));
+
+// Habilitamos compresion
+app.use(compression());
+
+// Habilitamos acceso al formulario
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Habilitamos carpeta externa
+app.use(express.static('public'));
+
+// Habilitamos plugins externos
+app.use('/assets', [
+  express.static(__dirname + '/node_modules/jquery/dist/'),
+  express.static(__dirname + '/node_modules/material-design-lite/dist/'),
+  express.static(__dirname + '/node_modules/normalize.css/'),
+  express.static(__dirname + '/node_modules/animate.css/'),
+  express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'),
+  express.static(__dirname + '/node_modules/cookieconsent/build/'),
+]);
+
+app.use(cookieParser('keyboard cat'));
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+  }),
+);
+
+// Habilitamos servidor
+const server = http.createServer(app);
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Mail Configuration
+app.post('/enviar-mensaje', function(req, res) {
+  if (
+    req.body['g-recaptcha-response'] === undefined ||
+    req.body['g-recaptcha-response'] === '' ||
+    req.body['g-recaptcha-response'] === null
+  ) {
+    return res.json({ responseError: 'Please select captcha first' });
+  }
+  const secretKey = '6LdeU40UAAAAAKOoSJoxLUIWFOKB8XZ9LLkoAjAh';
+
+  const verificationURL =
+    'https://www.google.com/recaptcha/api/siteverify?secret=' +
+    secretKey +
+    '&response=' +
+    req.body['g-recaptcha-response'] +
+    '&remoteip=' +
+    req.connection.remoteAddress;
+
+  request(verificationURL, function(error, res, body) {
+    body = JSON.parse(body);
+
+    if (body.success !== undefined && !body.success) {
+      return res.json({ responseError: 'Failed captcha verification' });
+    }
+
+    res.json({ responseSuccess: 'Sucess' });
+
+    console.log(res);
+
+    mail({
+      from: req.body.txtMail,
+      to: 'lavadoestadio@gmail.com',
+      subject: 'Mensaje de ' + req.body.txtNombrels + ' desde lavadoestadio.es',
+      html:
+        '<p><h4>Nombre:</h4> ' +
+        req.body.txtNombre +
+        '</p>' +
+        '<p><h4>Correo Electrónico</h4> ' +
+        req.body.txtMail +
+        '</p>' +
+        '</p><h4>Mensaje</h4> ' +
+        req.body.txtMensaje +
+        '</p>',
+    });
+  });
+});
---
+++ public/assets/js/custom.js
@@ -0,0 +1,65 @@
+function UpdateCopyYear() {
+  var d = new Date();
+  document.getElementById('copy-year').innerHTML = d.getFullYear();
+}
+
+$(document).ready(function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(function(reg) {
+        console.log('Service Worker registered successfully');
+      })
+      .catch(function(err) {
+        console.log('Service worker registration failed');
+      });
+  }
+
+  window.addEventListener('load', function() {
+    window.cookieconsent.initialise({
+      palette: {
+        popup: {
+          background: '#237afc',
+        },
+        button: {
+          background: '#fff',
+          text: '#237afc',
+        },
+      },
+      theme: 'edgeless',
+      content: {
+        message: 'Esta web usa cookies para mejorar la experiencia del usuario.',
+        dismiss: 'Entendido',
+        link: 'Más información',
+      },
+    });
+  });
+
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    (i[r] =
+      i[r] ||
+      function() {
+        (i[r].q = i[r].q || []).push(arguments);
+      }),
+      (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-55424484-5', 'auto');
+  ga('send', 'pageview');
+
+  window.cookieconsent_options = {
+    message: 'Esta web usa cookies para mejorar la experiencia del usuario',
+    dismiss: 'Entendido',
+    learnMore: 'Más info',
+    link: 'https://www.google.com/about/company/user-consent-policy.html',
+    theme: 'light-bottom',
+  };
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  UpdateCopyYear();
+});
